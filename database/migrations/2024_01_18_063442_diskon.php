ddb7b4e964259a5ab7af90eb9d031ecc9926ce15
umam.c.u23 <umam.c.u23@gmail.com>
2024-01-18 13:49:40 +0700

    Episode 2 - Migration - Setting env - Membuat Migration Tabel Barang, Jenis Barang, Transaksi, Detail Transaksi, Diskon - Membuat UserSeeder - Setting User Model

--- database/migrations/2024_01_18_063442_diskon.php
+++ database/migrations/2024_01_18_063442_diskon.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tbl_diskon', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['admin', 'kasir'])->nullable();
+            $table->bigInteger('total_belanja');
+            $table->integer('diskon')->nullable();
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tbl_diskon');
     }
 };
